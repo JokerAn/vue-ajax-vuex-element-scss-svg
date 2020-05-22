e5f74085fc094f47b5597438039a58208fb42ddc
JokerAn <divfooter@163.com>
2020-05-22 11:35:08 +0800

    圣杯布局

--- src/views/info-release/article-management/index.vue
+++ src/views/info-release/article-management/index.vue
@@ -4,7 +4,39 @@
 
 <script>
 export default {
+  data(){
+    return{
+      msg: '我是msg'
+    }
+  },
+  created(){
+    let a = [1,2,3,4]
 
+    console.log(a.includes(4))
+    let b = {
+      a: 1,b: 2,c: 3
+    }
+
+    console.log(Object.keys(b))
+    console.log('asdf'.startsWith('c'))
+    this.test(2).then(result=>{
+      console.log(result)
+    })
+  },
+  methods: {
+    test(res){
+      return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+          if(res){
+            resolve(res)
+          }else{
+            reject('啥都没传')
+          }
+        },4000)
+        
+      })
+    }
+  }
 }
 </script>
 
